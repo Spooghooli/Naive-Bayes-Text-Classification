93d80b2de21261fd4530e8b2d1504ecd7736e707
Spooghooli <jim.vardakastanis@gmail.com>
2020-12-20 16:29:56 +0200

    1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 
-algorithm = input("Choose the algorithm you want the classification to use")
+algorithm = input("Choose the algorithm you want the classification to use: press '1' for the bayes algorithm")
+while():
+    if(algorithm==1):
+        print("1")
+    elif(algorithm==2):
+        print("2")
+    elif(algorithm==3):
+        print("3")
+
